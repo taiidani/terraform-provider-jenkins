e1668a1cf09549f056999646df4848f4dafc8960
Ryan Nixon <r.taiidani@gmail.com>
2020-08-16 13:19:32 -0700

    Don't need the "2"s anymore

--- example/credentials.tf
+++ example/credentials.tf
@@ -1,11 +1,11 @@
 resource jenkins_credential_username global {
-  name     = "global-username2"
+  name     = "global-username"
   username = "foo"
   # password = "barsoom"
 }
 
 resource jenkins_credential_username folder {
-  name     = "folder-username2"
+  name     = "folder-username"
   folder   = jenkins_folder.example.name
   username = "folder-foo"
   password = "barsoom"
