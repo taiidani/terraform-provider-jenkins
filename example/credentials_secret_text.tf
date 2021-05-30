edecbd72bd8ab933b3375e511cc666ccef760932
Ryan Nixon <r.taiidani@gmail.com>
2021-05-30 16:06:36 -0700

    Fix collision with example jenkins_credentials

--- example/credentials_secret_text.tf
+++ example/credentials_secret_text.tf
@@ -1,10 +1,10 @@
 resource "jenkins_credential_secret_text" "global" {
-  name   = "global-username"
+  name   = "global-secret-text"
   secret = "barsoom"
 }
 
 resource "jenkins_credential_secret_text" "folder" {
-  name   = "folder-username"
+  name   = "folder-secret-text"
   folder = jenkins_folder.example.id
   secret = "barsoom"
 }
