7afc5a90710c1a916bd69047e7c90d14cecd748d
Florian Bauer <14075033+gitflo1@users.noreply.github.com>
2023-01-09 21:16:46 +0100

    Use azure_service_üprincipal_test_folder for testing.

--- example/credentials_azure_service_principal.tf
+++ example/credentials_azure_service_principal.tf
@@ -5,7 +5,7 @@ resource "jenkins_folder" "azure_service_principal_test_folder" {
     
 resource "jenkins_credential_azure_service_principal" "azure_service_principal_test_credential" {
     name = "bla"
-    folder = jenkins_folder.example.id
+    folder = jenkins_folder.azure_service_principal_test_folder.id
     description = "blabla"
     subscription_id = "123"
     client_id = "123"
