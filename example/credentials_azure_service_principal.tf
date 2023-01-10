37221d2add814ab717a68520eda427aa394fcb90
Florian <14075033+gitflo1@users.noreply.github.com>
2023-01-10 07:39:21 +0100

    Remove additional test folder and use example

--- example/credentials_azure_service_principal.tf
+++ example/credentials_azure_service_principal.tf
@@ -1,11 +1,6 @@
-resource "jenkins_folder" "azure_service_principal_test_folder" {
-    name        = "azure-service-principal-test-folder"
-    description = "A sample folder"
-}
-    
-resource "jenkins_credential_azure_service_principal" "azure_service_principal_test_credential" {
+   resource "jenkins_credential_azure_service_principal" "azure_service_principal_test_credential" {
     name = "bla"
-    folder = jenkins_folder.azure_service_principal_test_folder.id
+    folder = jenkins_folder.example.id
     description = "blabla"
     subscription_id = "123"
     client_id = "123"
