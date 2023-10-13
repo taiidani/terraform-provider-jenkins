8f60617f6366ecf2a4f16899ca4025bb628b3a4a
Ryan Nixon <r.taiidani@gmail.com>
2023-10-12 23:17:28 -0700

    Migrate SSH credential to Framework

--- jenkins/resource_jenkins_credential_ssh.go
+++ jenkins/resource_jenkins_credential_ssh.go
@@ -6,205 +6,247 @@ import (
 	"strings"
 
 	jenkins "github.com/bndr/gojenkins"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func resourceJenkinsCredentialSSH() *schema.Resource {
-	return &schema.Resource{
-		CreateContext: resourceJenkinsCredentialSSHCreate,
-		ReadContext:   resourceJenkinsCredentialSSHRead,
-		UpdateContext: resourceJenkinsCredentialSSHUpdate,
-		DeleteContext: resourceJenkinsCredentialSSHDelete,
-		Importer: &schema.ResourceImporter{
-			StateContext: resourceJenkinsCredentialSSHImport,
-		},
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:        schema.TypeString,
-				Description: "The identifier assigned to the credentials.",
-				Required:    true,
-				ForceNew:    true,
-			},
-			"domain": {
-				Type:        schema.TypeString,
-				Description: "The domain namespace that the credentials will be added to.",
-				Optional:    true,
-				Default:     defaultCredentialDomain,
-				// In-place updates should be possible, but gojenkins does not support move operations
-				ForceNew: true,
-			},
-			"folder": {
-				Type:        schema.TypeString,
-				Description: "The folder namespace that the credentials will be added to.",
-				Optional:    true,
-				ForceNew:    true,
-			},
-			"scope": {
-				Type:             schema.TypeString,
-				Description:      "The Jenkins scope assigned to the credentials.",
-				Optional:         true,
-				Default:          "GLOBAL",
-				ValidateDiagFunc: validateCredentialScope,
-			},
-			"description": {
-				Type:        schema.TypeString,
-				Description: "The credentials descriptive text.",
-				Optional:    true,
-				Default:     "Managed by Terraform",
-			},
-			"username": {
-				Type:        schema.TypeString,
-				Description: "Username",
-				Required:    true,
-			},
-			"privatekey": {
-				Type:        schema.TypeString,
-				Description: "The credentials private SSH key. This is mandatory.",
-				Required:    true,
-				Sensitive:   true,
-			},
-			"passphrase": {
-				Type:        schema.TypeString,
-				Description: "Passphrase for SSH key.",
-				Optional:    true,
-				Sensitive:   true,
-			},
-		},
+type credentialSSHResourceModel struct {
+	ID          types.String `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Folder      types.String `tfsdk:"folder"`
+	Description types.String `tfsdk:"description"`
+	Domain      types.String `tfsdk:"domain"`
+	Scope       types.String `tfsdk:"scope"`
+	Username    types.String `tfsdk:"username"`
+	PrivateKey  types.String `tfsdk:"privatekey"`
+	Passphrase  types.String `tfsdk:"passphrase"`
+}
+
+type credentialSSHResource struct {
+	*resourceHelper
+}
+
+// Ensure the implementation satisfies the desired interfaces.
+var _ resource.ResourceWithConfigure = &credentialSSHResource{}
+
+func newCredentialSSHResource() resource.Resource {
+	return &credentialSSHResource{
+		resourceHelper: newResourceHelper(),
 	}
 }
 
-func resourceJenkinsCredentialSSHCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(jenkinsClient)
-	cm := client.Credentials()
-	cm.Folder = formatFolderName(d.Get("folder").(string))
+// Metadata should return the full name of the resource.
+func (r *credentialSSHResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_credential_ssh"
+}
+
+// Schema should return the schema for this resource.
+func (r *credentialSSHResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: `
+Manages a SSH credential within Jenkins. This SSH credential may then be referenced within jobs that are created.
+
+~> The "passphrase" and "privatekey" properties may leave plain-text values in your state file. Ensure that your state file is properly secured and encrypted at rest.`,
+		Attributes: r.schemaCredential(map[string]schema.Attribute{
+			"username": schema.StringAttribute{
+				MarkdownDescription: "Username",
+				Required:            true,
+			},
+			"privatekey": schema.StringAttribute{
+				MarkdownDescription: "Private SSH key, can be given as string or read from file with 'file()' terraform function.",
+				Required:            true,
+				Sensitive:           true,
+			},
+			"passphrase": schema.StringAttribute{
+				MarkdownDescription: "Passphrase for privatekey. This has to be skipped if private key was created without passphrase.",
+				Optional:            true,
+				Sensitive:           true,
+			},
+		}),
+	}
+}
+
+// Create is called when the provider must create a new resource. Config
+// and planned state values should be read from the
+// CreateRequest and new state values set on the CreateResponse.
+func (r *credentialSSHResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data credentialSSHResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	cm := r.client.Credentials()
+	cm.Folder = formatFolderName(data.Folder.ValueString())
 
 	// Validate that the folder exists
-	if err := folderExists(ctx, client, cm.Folder); err != nil {
-		return diag.FromErr(fmt.Errorf("invalid folder name '%s' specified: %w", cm.Folder, err))
+	if err := folderExists(ctx, r.client, cm.Folder); err != nil {
+		resp.Diagnostics.AddError(
+			"Invalid Folder",
+			fmt.Sprintf("An invalid folder name %q was specified. ", cm.Folder)+
+				"Please report this issue to the provider developers.\n\n"+
+				"Error: "+err.Error(),
+		)
+
+		return
 	}
 
 	cred := jenkins.SSHCredentials{
-		ID:          d.Get("name").(string),
-		Scope:       d.Get("scope").(string),
-		Description: d.Get("description").(string),
-		Username:    d.Get("username").(string),
+		ID:          data.Name.ValueString(),
+		Scope:       data.Scope.ValueString(),
+		Description: data.Description.ValueString(),
+		Username:    data.Username.ValueString(),
 		PrivateKeySource: &jenkins.PrivateKey{
 			Class: jenkins.KeySourceDirectEntryType,
-			Value: d.Get("privatekey").(string),
+			Value: data.PrivateKey.ValueString(),
 		},
 	}
 
-	passphrase := d.Get("passphrase").(string)
+	passphrase := data.Passphrase.ValueString()
 	if len(passphrase) > 0 {
 		cred.Passphrase = passphrase
 	}
 
-	domain := d.Get("domain").(string)
-	err := cm.Add(ctx, domain, cred)
+	err := cm.Add(ctx, data.Domain.ValueString(), cred)
 	if err != nil {
-		return diag.Errorf("Could not create ssh credentials: %s", err)
+		resp.Diagnostics.AddError(
+			"Unable to Create Resource",
+			"An unexpected error occurred while creating the resource. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"Error: "+err.Error(),
+		)
+
+		return
 	}
 
-	d.SetId(generateCredentialID(d.Get("folder").(string), cred.ID))
-	return resourceJenkinsCredentialSSHRead(ctx, d, meta)
+	// Convert from the API data model to the Terraform data model
+	// and set any unknown attribute values.
+	data.ID = types.StringValue(generateCredentialID(data.Folder.ValueString(), cred.ID))
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func resourceJenkinsCredentialSSHRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cm := meta.(jenkinsClient).Credentials()
-	cm.Folder = formatFolderName(d.Get("folder").(string))
+// Read is called when the provider must read resource values in order
+// to update state. Planned state values should be read from the
+// ReadRequest and new state values set on the ReadResponse.
+func (r *credentialSSHResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data credentialSSHResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	cm := r.client.Credentials()
+	cm.Folder = formatFolderName(data.Folder.ValueString())
 
 	cred := jenkins.SSHCredentials{}
-	err := cm.GetSingle(
-		ctx,
-		d.Get("domain").(string),
-		d.Get("name").(string),
-		&cred,
-	)
-
+	err := cm.GetSingle(ctx, data.Domain.ValueString(), data.Name.ValueString(), &cred)
 	if err != nil {
 		if strings.HasSuffix(err.Error(), "404") {
 			// Job does not exist
-			d.SetId("")
-			return nil
+			resp.State.RemoveResource(ctx)
+			return
 		}
 
-		return diag.Errorf("Could not read ssh credentials: %s", err)
+		resp.Diagnostics.AddError(
+			"Unable to Refresh Resource",
+			"An unexpected error occurred while parsing the resource read response. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"Error: "+err.Error(),
+		)
+
+		return
 	}
 
-	d.SetId(generateCredentialID(d.Get("folder").(string), cred.ID))
-	_ = d.Set("scope", cred.Scope)
-	_ = d.Set("description", cred.Description)
-	// NOTE: We are NOT setting the secret here, as the secret returned by GetSingle is garbage
-	// Secret only applies to Create/Update operations if the "password" property is non-empty
+	data.ID = types.StringValue(generateCredentialID(data.Folder.ValueString(), cred.ID))
+	data.Scope = types.StringValue(cred.Scope)
+	data.Description = types.StringValue(cred.Description)
+	data.Username = types.StringValue(cred.Username)
 
-	return nil
+	// NOTE: We are NOT setting the secrets here, as the secrets returned by GetSingle are garbage
+	// Secret only applies to Create/Update operations if the "privatekey" or "passphrase" properties are non-empty
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func resourceJenkinsCredentialSSHUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cm := meta.(jenkinsClient).Credentials()
-	cm.Folder = formatFolderName(d.Get("folder").(string))
+// Update is called to update the state of the resource. Config, planned
+// state, and prior state values should be read from the
+// UpdateRequest and new state values set on the UpdateResponse.
+func (r *credentialSSHResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data credentialSSHResourceModel
 
-	domain := d.Get("domain").(string)
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	cm := r.client.Credentials()
+	cm.Folder = formatFolderName(data.Folder.ValueString())
 
 	cred := jenkins.SSHCredentials{
-		ID:          d.Get("name").(string),
-		Scope:       d.Get("scope").(string),
-		Description: d.Get("description").(string),
-		Username:    d.Get("username").(string),
+		ID:          data.Name.ValueString(),
+		Scope:       data.Scope.ValueString(),
+		Description: data.Description.ValueString(),
+		Username:    data.Username.ValueString(),
 		PrivateKeySource: &jenkins.PrivateKey{
 			Class: jenkins.KeySourceDirectEntryType,
-			Value: d.Get("privatekey").(string),
+			Value: data.PrivateKey.ValueString(),
 		},
 	}
 
-	passphrase := d.Get("passphrase").(string)
-	if len(passphrase) > 0 {
-		cred.Passphrase = passphrase
+	// Only enforce the secrets if non-empty
+	if data.Passphrase.ValueString() != "" {
+		cred.Passphrase = data.Passphrase.ValueString()
 	}
 
-	err := cm.Update(ctx, domain, d.Get("name").(string), &cred)
+	err := cm.Update(ctx, data.Domain.ValueString(), data.Name.ValueString(), &cred)
 	if err != nil {
-		return diag.Errorf("Could not update secret text: %s", err)
+		resp.Diagnostics.AddError(
+			"Unable to Update Resource",
+			"An unexpected error occurred while attempting to update the resource. "+
+				"Please retry the operation or report this issue to the provider developers.\n\n"+
+				"Error: "+err.Error(),
+		)
+
+		return
 	}
 
-	d.SetId(generateCredentialID(d.Get("folder").(string), cred.ID))
-	return resourceJenkinsCredentialSSHRead(ctx, d, meta)
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func resourceJenkinsCredentialSSHDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	cm := meta.(jenkinsClient).Credentials()
-	cm.Folder = formatFolderName(d.Get("folder").(string))
+// Delete is called when the provider must delete the resource. Config
+// values may be read from the DeleteRequest.
+//
+// If execution completes without error, the framework will automatically
+// call DeleteResponse.State.RemoveResource(), so it can be omitted
+// from provider logic.
+func (r *credentialSSHResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data credentialSSHResourceModel
 
-	err := cm.Delete(
-		ctx,
-		d.Get("domain").(string),
-		d.Get("name").(string),
-	)
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	cm := r.client.Credentials()
+	cm.Folder = formatFolderName(data.Folder.ValueString())
+
+	err := cm.Delete(ctx, data.Domain.ValueString(), data.Name.ValueString())
 	if err != nil {
-		return diag.FromErr(err)
+		resp.Diagnostics.AddError(
+			"Unable to Delete Resource",
+			"An unexpected error occurred while deleting the resource. "+
+				"Please report this issue to the provider developers.\n\n"+
+				"Error: "+err.Error(),
+		)
+
+		return
 	}
-
-	return nil
-}
-
-func resourceJenkinsCredentialSSHImport(ctx context.Context, d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
-	ret := []*schema.ResourceData{d}
-
-	splitID := strings.Split(d.Id(), "/")
-	if len(splitID) < 2 {
-		return ret, fmt.Errorf("import ID was improperly formatted. Imports need to be in the format \"[<folder>/]<domain>/<name>\"")
-	}
-
-	name := splitID[len(splitID)-1]
-	_ = d.Set("name", name)
-
-	domain := splitID[len(splitID)-2]
-	_ = d.Set("domain", domain)
-
-	folder := strings.Trim(strings.Join(splitID[0:len(splitID)-2], "/"), "/")
-	_ = d.Set("folder", folder)
-
-	d.SetId(generateCredentialID(folder, name))
-	return ret, nil
 }
