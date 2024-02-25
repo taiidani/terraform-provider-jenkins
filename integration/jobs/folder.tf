1437c53c875844f1175aed96c03c8a452a3a6e33
Ryan Nixon <902500+taiidani@users.noreply.github.com>
2024-02-25 11:03:06 -0800

    Add integration test assertions for folders (#204)

--- integration/jobs/folder.tf
+++ integration/jobs/folder.tf
@@ -12,8 +12,19 @@ resource "jenkins_folder" "example" {
   }
 }
 
+data "jenkins_folder" "example" {
+  depends_on = [jenkins_folder.example]
+  name       = jenkins_folder.example.name
+}
+
 resource "jenkins_folder" "example_subfolder" {
   name        = "subfolder"
   folder      = jenkins_folder.example.id
   description = "A sample subfolder"
+}
+
+data "jenkins_folder" "example_subfolder" {
+  depends_on = [jenkins_folder.example_subfolder]
+  name       = jenkins_folder.example_subfolder.name
+  folder     = jenkins_folder.example_subfolder.folder
 }
